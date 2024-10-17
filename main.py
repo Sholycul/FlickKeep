1420213eb477e4022d893d3f1440ecffd56a037c
Sholycul <michaeloluwasholaiji2020@gmail.com>
2024-10-17 02:04:17 +0100

    Remove Unique constraint on username

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ login_manager.login_view = 'login'
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(50), nullable=False)
     movies = db.Relationship('Movie', backref='user', lazy=True)
 
